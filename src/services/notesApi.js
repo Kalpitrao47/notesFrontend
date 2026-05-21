4ecd3de6fedbd1ca5f145e500c0ead9e8b78ec4e
Tanmay Sawant <tanmay.s@unlocktech.co.in>
2026-05-21 18:49:40 +0530

    app.js 2

--- src/services/notesApi.js
+++ src/services/notesApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://notes-backend-pearl-alpha.vercel.app";
+const BASE_URL = "https://notes-backend-pearl-alpha.vercel.app";
 
 export const getNotes = async () => {
   const response = await fetch(`${BASE_URL}/notes/list`);
@@ -42,7 +42,7 @@ export const createNote = async ({
 
 export const updateNote = async (_id, noteData) => {
   const response = await fetch(
-    `http://notes-backend-pearl-alpha.vercel.app/update/notes/${_id}`,
+    `https://notes-backend-pearl-alpha.vercel.app/update/notes/${_id}`,
     {
       method: "PUT",
       headers: {
@@ -62,7 +62,7 @@ export const updateNote = async (_id, noteData) => {
 };
 
 export const permanentDelete = async (_id) => {
-  const response = await fetch(`http://notes-backend-pearl-alpha.vercel.app/delete/notes/${_id}`, {
+  const response = await fetch(`https://notes-backend-pearl-alpha.vercel.app/delete/notes/${_id}`, {
     method: "DELETE",
   });
 
